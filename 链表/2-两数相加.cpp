a9dabe19ac1ca88bb4af9c164ca9fcc47a960704
sunnyzhang06 <sunnyzhang9205@gmail.com>
2019-05-14 15:14:02 +0800

    amend number 2

--- 链表/2-两数相加.cpp
+++ 链表/2-两数相加.cpp
@@ -35,7 +35,33 @@ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
 	return head->next;
 }
 
-
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+{
+	if(l1==NULL)
+		return l2;
+	if(l2==NULL)
+		return l1;
+	
+	ListNode* head = new ListNode(0);
+	ListNode* cur = head;
+	int carry = 0;
+	
+	while(l1 || l2)
+	{
+		int val1 = l1?l1->val:0;
+		int val2 = l2?l2->val:0;
+		int sum = val1 + val2 + carry;
+		carry = sum/10;
+		cur->next = new ListNode(sum % 10);
+		cur = cur->next;
+		
+		l1 = l1?l1->next:l1;
+		l2 = l2?l2->next:l2;
+	}
+	if(carry)
+		cur->next = new ListNode(1);
+	return head->next;
+}
 
 
 
