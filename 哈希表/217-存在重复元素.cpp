3eb42ada4c2a80f7c8e192793f9b364320a30538
sunnyzhang06 <sunnyzhang9205@gmail.com>
2019-08-17 18:02:09 +0800

    add number 217

--- 哈希表/217-存在重复元素.cpp
+++ 哈希表/217-存在重复元素.cpp
@@ -1,16 +1,39 @@
+/*
+* @author：sunny
+* 217、存在重复元素
+* https://leetcode-cn.com/problems/contains-duplicate/
+* 给定一个整数数组，判断是否存在重复元素。
+*/
 
-5//217存在重复元素
-//如果任何值在数组中出现至少两次，函数返回 true
+/*
+* 方法1：用 set
+* 思路：如果字符没出现过就加入到集合中，如果出现过就返回true；
+*/
 
- bool containsDuplicate(vector<int>& nums)
- {
-	//map统计每个数字出现的次数，{num：count}
+bool containsDuplicate(vector<int>& nums)
+{
+	unordered_set<int> set;
+	for(auto num:nums)
+	{
+		if(st.count(num))
+			return true;
+		st.insert(num);
+	}
+	return false;
+}
+
+/*
+* 方法2：用 map 建立数字到出现次数的映射 
+* 思路：map统计每个数字出现的次数，{num：count}
+*/
+bool containsDuplicate(vector<int>& nums)
+{
 	unordered_map<int,int> m;
-	 for(int i=0;i<nums.size();++i)
-	 {
-		 if(m.find(nums[i])!=m.end())//查找是否出现过
-			 return true;
-		 ++m[nums[i]];//记录nums[i]及其出现的次数
-	 }
-	 return false;
- }
+	for(auto num:nums)
+	{
+	 if(m.find(num)!=m.end())//查找是否出现过,或写成 if(m.count(num))
+		 return true;
+	 ++m[num];//记录nums[i]及其出现的次数
+	}
+	return false;
+}
